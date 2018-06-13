757960a212f7806a2be2e1d2979ee2b92c610d33
samwolde <samuelwoldemariam@yahoo.com>
2018-06-13 18:35:43 +0300

    Get team announcements, projects and members tested

--- app/src/main/java/com/ux7/fullhyve/services/Models/Team.java
+++ app/src/main/java/com/ux7/fullhyve/services/Models/Team.java
@@ -2,9 +2,7 @@ package com.ux7.fullhyve.services.Models;
 
 import java.io.Serializable;
 
-/**
- * Created by TOSHIBA on 4/21/2018.
- */
+
 public class Team implements Serializable{
     public int id;
     public String name;
